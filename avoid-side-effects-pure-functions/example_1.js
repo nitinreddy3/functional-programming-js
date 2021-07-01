ffbd0e6df64e12e5e74ab9a77c79d0910ea1bf5a
Nitin Reddy <nitin.reddy@appdirect.com>
2021-07-01 16:39:31 +0530

    Added the examples which explains about how to avoid side effects

---
+++ avoid-side-effects-pure-functions/example_1.js
@@ -0,0 +1,12 @@
+// Example 1
+
+let count = 0;
+
+let increment = () => {
+	count++;
+	return count;
+}
+
+console.log(increment());
+console.log(count);
+
---
+++ avoid-side-effects-pure-functions/example_2.js
@@ -0,0 +1,13 @@
+let x = 1;
+
+foo1()
+
+console.log(x)
+
+foo2()
+
+console.log(x)
+
+foo3()
+
+console.log(x)

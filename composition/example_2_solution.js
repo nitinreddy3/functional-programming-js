bf9411312430c3b9703681f495e5e19a629425e1
nitin reddy <reddynitin3@gmail.com>
2021-07-04 21:41:12 +0530

    Updated the pipe function

--- composition/example_2_solution.js
+++ composition/example_2_solution.js
@@ -12,9 +12,10 @@ const noArticles = str => (str !== 'A' && str !== 'AN' && str !== 'THE');
 
 const filterArticles = arr => arr.filter(noArticles);
 
-const compose = (...fns) => (x) => fns.reduceRight((value, currentFunction) => currentFunction(value), x);
+const compose = (...fns) => (x) => fns.reduce((value, currentFunction) => currentFunction(value), x);
 
-const prepareString = compose(filterArticles, splitStr, capitalizeStr, noPunction, trimString)
+// const prepareString = compose1(filterArticles, splitStr, capitalizeStr, noPunction, trimString)
+const prepareString = compose(trimString, noPunction, capitalizeStr, splitStr, filterArticles);
 
 console.log(filterArticles(splitStr(capitalizeStr(noPunction(trimString(str)))))); // option 1
-console.log(prepareString(str)); // option 1
+console.log(prepareString(str)); // option 2
